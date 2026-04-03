49960adb0a654e09b8c7c7bb048936716809bc2c
slowmo0417 <kka8776@gmail.com>
2026-04-03 09:20:07 +0900

    Add files via upload

---
+++ 18_formdatA/script/script.js
@@ -0,0 +1,17 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyocCXnPBryvzDDTYv8F77_0fQu6fnFgx5FafHqmhaEz24hHclcVpaRtAg6uAXCFgKa/exec"; // add your own app script link here
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
